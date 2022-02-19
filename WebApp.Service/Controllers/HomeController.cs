01c58c46b25822fde031fd66c165be6232a09d7c
Jacek Nowicki <mopikx@gmail.com>
2022-02-19 09:32:09 +0100

    Add project files.

---
+++ WebApp.Service/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Service.Controllers
+{
+    [ApiController]
+    [Route("HomeController")]
+    public class HomeController : ControllerBase
+    {
+        
+        [HttpGet]
+        public IEnumerable<int> Get()
+        {
+            return new[]
+            {
+                1, 2, 3, 4, 5, 6
+            };
+        }
+    }
+}
